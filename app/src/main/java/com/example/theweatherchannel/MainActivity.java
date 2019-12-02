97e85de4c4d6c14d00afdc37c5c5ef982177d91b
RasmusOberg <44671186+RasmusOberg@users.noreply.github.com>
2019-12-02 22:19:22 +0100

    test

--- app/src/main/java/com/example/theweatherchannel/MainActivity.java
+++ app/src/main/java/com/example/theweatherchannel/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.theweatherchannel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -19,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
     private Sensor pressure, temp, humidity;
     private boolean tempPresent, pressurePresent, humidityPresent;
     private TextView information;
+    private double longitude, latitude;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private String api;
+    private int test = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +48,48 @@ public class MainActivity extends AppCompatActivity {
         sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
         listener = new Listener(this);
         information = findViewById(R.id.information);
+        getLongitudeLatitude();
+        getSensors();
+//        new FetchWeatherInformation().execute("api.openweathermap.org/data/2.5/weather?lat=35&lon=139");
+    }
 
+    public void getLongitudeLatitude() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener(){
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                Log.d(TAG, "onLocationChanged: " + latitude + " " + longitude);
+                api = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&APPID=868caba3626811db78521241d52982a3";
+                Log.d(TAG, "onLocationChanged: " + api);
+                new FetchWeatherInformation().execute(api);
+            }
+
+            public void onProviderDisabled(String provider) { }
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+        };
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
+
+    public void getSensors(){
         if (sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null) {
             temp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
             tempPresent = true;
@@ -52,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "onCreate: Pressure Sensor not available.");
             pressurePresent = false;
         }
-        new FetchWeatherInformation().execute("api.openweathermap.org/data/2.5/weather?lat=35&lon=139");
     }
 
     protected void onResume() {
@@ -81,10 +142,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class FetchWeatherInformation extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... strings) {
-            return null;
+        protected String doInBackground(String... urls) {
+            Log.d(TAG, "doInBackground: " + urls[0]);
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream is = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(is);
+                int data = reader.read();
+                while(data != -1){
+                    char current = (char)data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d(TAG, "doInBackground: "+ e.getMessage());
+                return null;
+            }
         }
 
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try{
+                String object = new JSONObject(s).getString("weather"); //array with general information
+                Log.d(TAG, "onPostExecute: Weather = " + object);
+                String obj2 = new JSONObject(s).getString("main");
+                Log.d(TAG, "onPostExecute: main = " + obj2);
+            }catch(JSONException e){
+                Log.d(TAG, "onPostExecute: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 }
